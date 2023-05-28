91758ed61427a4ab436c0a8f290105bab8de3382
Lukman Prayogi <lukman.prayogi@spesolution.com>
2023-05-28 21:11:03 +0700

    section 4

---
+++ JS_Variable/const.js
@@ -0,0 +1,16 @@
+// const  nama = "iqbal";
+// nama = "niyaz";
+// console.log(nama);
+
+// const mutable with object
+// const mahasiswa =  {
+//     nama: 'iqbal',
+//     kelas: '20A',
+// }
+// mahasiswa.fakultas = 'Informatika'
+// console.log (mahasiswa);
+
+//  const mutable with array
+const mahasiswa = ['iqbal', 'niyaz', 'eka'];
+mahasiswa.push('dwi');
+console.log(mahasiswa);
---
+++ JS_Variable/tugas_variable.js
@@ -0,0 +1,3 @@
+let x = "10" + 10 + 10;
+let y = 10 + 10 + "10";
+console.log(x+y);
